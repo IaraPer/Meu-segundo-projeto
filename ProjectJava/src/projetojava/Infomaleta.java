e727637d080d469bcdeb66fda5d8a90a32e829a7
IARA <IARAPER>
2023-11-18 21:25:11 -0300

    Minha segunda commit

---
+++ ProjectJava/src/projetojava/Infomaleta.java
@@ -0,0 +1,15 @@
+package projetojava;
+
+public class Infomaleta {
+
+	public static void main(String[] args) {
+		Maleta maleta = new Maleta();
+		
+		maleta.tamanho = "G";
+		maleta.cor = "Rosa";
+		
+		maleta.infomala();
+		maleta.localizacaomaleta();
+	}
+
+}
---
+++ ProjectJava/src/projetojava/Maleta.java
@@ -0,0 +1,16 @@
+package projetojava;
+
+public class Maleta {
+	String cor;
+	String tamanho;
+	
+	void infomala() {
+		System.out.println("Tamanho: " + tamanho);
+		System.out.println("Cor: " + cor);
+	}
+	
+	void localizacaomaleta() {
+		System.out.println("A maleta está no bagageiro!");
+	}
+
+}
